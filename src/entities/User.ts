fa4f0435d629d204600db09cea52531204b915eb
m9dnts8608 <leah8608@gmail.com>
2020-08-15 04:45:42 +0900

    Chat and Message

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -5,11 +5,16 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  ManyToOne,
+  OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
   BeforeInsert,
   BeforeUpdate,
 } from "typeorm";
+
+import Chat from "./Chat";
+import Message from "./Message";
 
 const BCRYPT_ROUNDS = 10;
 
@@ -62,6 +67,12 @@ class User extends BaseEntity {
 
   @Column({ type: "double precision", default: 0 })
   lastOrientation: number;
+
+  @ManyToOne((type) => Chat, (chat) => chat.participants)
+  chat: Chat;
+
+  @OneToMany((type) => Message, (message) => message.user)
+  message: Message[];
 
   @CreateDateColumn() createdAt: string;
   @UpdateDateColumn() updatedAt: string;
