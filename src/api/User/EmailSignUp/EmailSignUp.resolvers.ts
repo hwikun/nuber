499e7a399f3f303b82626ea8c36df2a5c71bdded
m9dnts8608 <leah8608@gmail.com>
2020-08-15 09:41:24 +0900

    Authenticating Users with Custom JWT

--- src/api/User/EmailSignUp/EmailSignUp.resolvers.ts
+++ src/api/User/EmailSignUp/EmailSignUp.resolvers.ts
@@ -1,9 +1,10 @@
-import { Resolvers } from "../../types/resolvers";
+import { Resolvers } from "../../../types/resolvers";
 import {
   EmailSignUpMutationArgs,
   EmailSignUpResponse,
-} from "../../types/graph";
-import User from "../../entities/User";
+} from "../../../types/graph";
+import User from "../../../entities/User";
+import createJWT from "../../../utils/createJWT";
 
 const resolvers: Resolvers = {
   Mutation: {
@@ -22,10 +23,11 @@ const resolvers: Resolvers = {
           };
         } else {
           const newUser = await User.create({ ...args }).save();
+          const token = createJWT(newUser.id);
           return {
             ok: true,
             error: null,
-            token: "Coming soon",
+            token,
           };
         }
       } catch (error) {
